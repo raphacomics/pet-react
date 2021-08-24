64d3d9798c04dc15fbef6e08c20f593cd8217bc1
Rapha Lobosco <83170908+raphacomics@users.noreply.github.com>
2021-08-24 16:19:50 +0100

    Post working

--- prapet/schemas/post.js
+++ prapet/schemas/post.js
@@ -1,6 +1,6 @@
 export default {
   name: 'post',
-  title: 'Post',
+  title: 'Produtos',
   type: 'document',
   fields: [
     {
--- src/components/Produtos.js
+++ src/components/Produtos.js
@@ -1,8 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
+import sanityClient from "../client.js";
+
 
 
 export default function Produtos() {
+
+    const [prodData, setProd] = useState(null);    
+
+    useEffect( () => {
+        sanityClient
+            .fetch(
+                `*[_type == "post"]{
+                    title,
+                    slug,
+                    mainImage{
+                        asset->{
+                            _id,
+                            url
+                        },
+                        alt
+                    }
+                }`)
+                .then((data) => setProd(data))
+                .catch(console.error)
+    }, [])
+
     return (
-        
+        <main>
+            <section className="container">
+                <h1>Produtos</h1>
+                <h2>Veja os produtos da PraPet</h2>
+                <div className="row">
+                    {prodData && prodData.map((post, index) => (
+                        <article className="col-lg-3 col-md-4 col-sm-6">
+                            <Link to={"/produtos/" + post.slug.current} key={post.slug.current}>
+                            <span key={index}>
+                                <img className="img-fluid" src={post.mainImage.asset.url} alt={post.mainImage.alt} />
+                                <span>
+                                    <h4>{post.title}</h4>
+                                </span>
+                            </span>
+                            </Link>
+                        </article>
+                    ))}
+                </div>
+            </section>
+        </main>
     )
 }
